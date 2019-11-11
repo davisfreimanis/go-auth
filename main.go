cedb0e2e9eb034b135646edca22d58aac1b18e18
Davis Freimanis <davisfreimanis@gmail.com>
2019-11-11 21:02:41 +0100

    Handle static pages

--- main.go
+++ main.go
@@ -12,7 +12,7 @@ var counter int
 var mutex = &sync.Mutex{}
 
 func echoString(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, "hello")
+	fmt.Fprint(w, "Hi!")
 }
 
 func incrementCounter(w http.ResponseWriter, r *http.Request) {
@@ -23,13 +23,11 @@ func incrementCounter(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
-	http.HandleFunc("/", echoString)
+	http.Handle("/", http.FileServer(http.Dir("./static")))
 
 	http.HandleFunc("/increment", incrementCounter)
 
-	http.HandleFunc("/hi", func(writer http.ResponseWriter, request *http.Request) {
-		fmt.Fprint(writer, "Hello")
-	})
+	http.HandleFunc("/hi", echoString)
 
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
