4cc23a91f74948f2863d6365adf8b589def38865
Davis Freimanis <davisfreimanis@gmail.com>
2019-11-11 20:42:59 +0100

    Increment counter every time page is visited

--- main.go
+++ main.go
@@ -2,15 +2,30 @@ package main
 
 import (
 	"fmt"
-	"html"
 	"log"
 	"net/http"
+	"strconv"
+	"sync"
 )
 
+var counter int
+var mutex = &sync.Mutex{}
+
+func echoString(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "hello")
+}
+
+func incrementCounter(w http.ResponseWriter, r *http.Request) {
+	mutex.Lock()
+	counter++
+	fmt.Fprintf(w, strconv.Itoa(counter))
+	mutex.Unlock()
+}
+
 func main() {
-	http.HandleFunc("/", func(writer http.ResponseWriter, request *http.Request) {
-		fmt.Fprintf(writer, "Hello, %q", html.EscapeString(request.URL.Path))
-	})
+	http.HandleFunc("/", echoString)
+
+	http.HandleFunc("/increment", incrementCounter)
 
 	http.HandleFunc("/hi", func(writer http.ResponseWriter, request *http.Request) {
 		fmt.Fprint(writer, "Hello")
